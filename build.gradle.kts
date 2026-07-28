1d406ab277a5d82b42f73cb775dfe83df2d6119f
João Ferreira <joao.ferreira@actito.com>
2026-07-28 15:23:38 +0100

    feat: update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.androidx.navigation) apply false
     alias(libs.plugins.google.hilt) apply false
