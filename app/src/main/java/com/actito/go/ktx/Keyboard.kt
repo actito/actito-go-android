ff191534a30b8f3eaef1493a0800c20b021e1ed7
João Ferreira <joao.ferreira@actito.com>
2026-07-29 16:21:03 +0100

    refactor: resolve deprecations

--- app/src/main/java/com/actito/go/ktx/Keyboard.kt
+++ app/src/main/java/com/actito/go/ktx/Keyboard.kt
@@ -10,7 +10,6 @@ import android.view.ViewTreeObserver.OnGlobalFocusChangeListener
 import android.view.Window
 import android.view.WindowInsets
 import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
 import androidx.core.content.getSystemService
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsCompat.Type.ime
@@ -59,37 +58,28 @@ public inline fun Activity.setOnKeyboardVisibilityChangeListener(
  *
  * The [imm] is resolved with [getSystemService] when not provided.
  *
- * Set [isHideImplicitOnly] to `true` to set the [HIDE_IMPLICIT_ONLY]
- * flag for [InputMethodManager.hideSoftInputFromWindow].
- *
  * @throws NullPointerException when [InputMethodManager] is not found.
  */
 public inline fun Activity.hideKeyboardOnFocusChange(
-    isHideImplicitOnly: Boolean = false,
     imm: InputMethodManager = getSystemService()!!,
     crossinline predicate: (newView: View) -> Boolean,
 ): OnGlobalFocusChangeListener = window.hideKeyboardOnFocusChange(
     imm,
-    isHideImplicitOnly,
     predicate,
 )
 
 /**
  * Configures the [Window] to hide the soft input method
  * when the [predicate] returns `true` for the `newView`.
- *
- * Set [isHideImplicitOnly] to `true` to set the [HIDE_IMPLICIT_ONLY]
- * flag for [InputMethodManager.hideSoftInputFromWindow].
  */
 public inline fun Window.hideKeyboardOnFocusChange(
     imm: InputMethodManager,
-    isHideImplicitOnly: Boolean = false,
     crossinline predicate: (newView: View) -> Boolean,
 ): OnGlobalFocusChangeListener = with(attributes.token) {
     OnGlobalFocusChangeListener { oldView, newView ->
         if (newView?.let(predicate) != false) imm.hideSoftInputFromWindow(
             (oldView ?: newView)?.windowToken ?: this,
-            if (isHideImplicitOnly) HIDE_IMPLICIT_ONLY else 0,
+            0,
         )
     }
 }.also(decorView.viewTreeObserver::addOnGlobalFocusChangeListener)
